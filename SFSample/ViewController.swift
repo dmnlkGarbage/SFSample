5ce7fa31523882513afaa0797726dae8d665b8ad
dmnlk <seikima2demon@gmail.com>
2016-12-31 15:01:08 +0900

    add uibutton

--- SFSample/ViewController.swift
+++ SFSample/ViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var launchButton: UIButton?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        launchButton = UIButton()
+        launchButton?.frame =  CGRect(x: 0, y: 0, width: 100, height: 20)
+        launchButton?.layer.masksToBounds = true
+        launchButton?.setTitle("Launch", for: UIControlState.normal)
+        launchButton?.backgroundColor = UIColor.black;
+        
+        launchButton?.layer.position = CGPoint(x: self.view.frame.width / 2, y: 200)
+        
+        launchButton?.addTarget(self, action: #selector(self.onClickLaunchButton(_:)), for: UIControlEvents.touchUpInside)
+        self.view.addSubview((launchButton)!);
     }
 
     override func didReceiveMemoryWarning() {
@@ -19,6 +30,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    internal func onClickLaunchButton(_ sender: AnyObject) {
+        
+    }
 
 }
 
