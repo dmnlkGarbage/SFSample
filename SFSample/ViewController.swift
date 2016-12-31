d829afcb129e7b8aa0afcbb2995efcf35d5eae48
dmnlk <seikima2demon@gmail.com>
2016-12-31 14:23:34 +0900

    Initial Commit

---
+++ SFSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SFSample
+//
+//  Created by 川口将貴 on 2016/12/31.
+//  Copyright © 2016年 dmnlk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
